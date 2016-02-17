4de7ac377b4f9c7a3ca3ce0d4b4c517c53cb636d
Yue Chi <yuechi@microsoft.com>
2016-02-17 15:36:01 -0800

    Change project name

---
+++ Create-Update-Project-Samples/Create-Update-Project-Samples/CreateUpdateProjectSample.cs
@@ -0,0 +1,105 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Net;
+
+using Microsoft.SharePoint.Client;
+using csom = Microsoft.ProjectServer.Client;
+
+namespace CreateUpdateProjectSample
+{
+    partial class CreateUpdateProjectSample
+    {
+        public static string pwaInstanceUrl = "http://localhost/pwa/";         // your pwa url
+        private static csom.ProjectContext context;
+        const int DEFAULTTIMEOUTSECONDS = 300;
+
+        private static string projectName = "New Project";
+        private static string localResourceName = "New Local Resource";
+        private static string taskName = "New Task";
+
+        private static string projectCFName = "Project custom field";
+        private static string resourceCFName = "Resource custom field";
+        private static string taskCFName = "Task custom field";
+
+        static void Main(string[] args)
+        {
+            CreateProjectWithTaskAndAssignment();
+            ReadAndUpdateProject();
+            UpdateCustomFieldValues();
+        }
+
+        #region Utility functions
+        /// <summary>
+        /// Log to Console the job state for queued jobs
+        /// </summary>
+        /// <param name="jobState">csom jobstate</param>
+        /// <param name="jobDescription">job description</param>
+        private static void JobStateLog(csom.JobState jobState, string jobDescription)
+        {
+            switch (jobState)
+            {
+                case csom.JobState.Success:
+                    Console.WriteLine(jobDescription + " is successfully done.");
+                    break;
+                case csom.JobState.ReadyForProcessing:
+                case csom.JobState.Processing:
+                case csom.JobState.ProcessingDeferred:
+                    Console.WriteLine(jobDescription + " is taking longer than usual.");
+                    break;
+                case csom.JobState.Failed:
+                case csom.JobState.FailedNotBlocking:
+                case csom.JobState.CorrelationBlocked:
+                    Console.WriteLine(jobDescription + " failed. The job is in state: " + jobState);
+                    break;
+                default:
+                    Console.WriteLine("Unkown error, job is in state " + jobState);
+                    break;
+            }
+        }
+
+        /// <summary>
+        /// Get Publish project by name
+        /// </summary>
+        /// <param name="name">the name of the project</param>
+        /// <param name="context">csom context</param>
+        /// <returns></returns>
+        private static csom.PublishedProject GetProjectByName(string name, csom.ProjectContext context)
+        {
+            IEnumerable<csom.PublishedProject> projs = context.LoadQuery(context.Projects.Where(p => p.Name == name));
+            context.ExecuteQuery();
+
+            if (!projs.Any())       // no project found
+            { 
+                return null;
+            }
+            return projs.FirstOrDefault();
+        }
+
+        /// <summary>
+        /// Get csom ProjectContext by letting user type in username and password
+        /// </summary>
+        /// <param name="url">pwa website url string</param>
+        /// <returns></returns>
+        private static csom.ProjectContext GetContext(string url)
+        {
+            csom.ProjectContext context = new csom.ProjectContext(url);
+            string userName, passWord;
+
+            Console.WriteLine("Please enter your username for PWA");
+            userName = Console.ReadLine();
+            Console.WriteLine("Please enter your password for PWA");
+            passWord = Console.ReadLine();
+
+            NetworkCredential netcred = new NetworkCredential(userName, passWord);
+            SharePointOnlineCredentials orgIDCredential = new SharePointOnlineCredentials(netcred.UserName, netcred.SecurePassword);
+            context.Credentials = orgIDCredential;
+
+            return context;
+        }
+
+        #endregion
+    }
+}
---
+++ Create-Update-Project-Samples/Create-Update-Project-Samples/UpdateProject.cs
@@ -0,0 +1,79 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Globalization;
+using System.Text;
+using System.Threading.Tasks;
+
+using Microsoft.SharePoint.Client;
+using csom = Microsoft.ProjectServer.Client;
+
+namespace CreateUpdateProjectSample
+{
+    partial class CreateUpdateProjectSample
+    {
+        /// <summary>
+        /// Read and update the project,
+        /// this method need a project named "New Project" with a task "New task" and assign to a local resource named "New local resource" already created.
+        /// Basically please run CreateProjectWithTaskAndAssignment() before running this to avoid exceptions
+        /// </summary>
+        public static void ReadAndUpdateProject()
+        {
+            // Load csom context
+            context = GetContext(pwaInstanceUrl);
+
+            // Retrieve publish project named "New Project"
+            // if you know the Guid of project, you can just call context.Projects.GetByGuid()
+            csom.PublishedProject project = GetProjectByName(projectName, context);
+            if(project == null)
+            {
+                Console.WriteLine("Failed to retrieve expected data, make sure you set up server data right. Press any key to continue....");
+                return;
+            }
+
+            csom.DraftProject draft = project.CheckOut();
+
+            // Retrieve project along with tasks & resources
+            context.Load(draft, p => p.StartDate,                                                         
+                                p => p.Description);                                                      
+            context.Load(draft.Tasks, dt => dt.Where(t => t.Name == taskName));                           
+            context.Load(draft.Assignments, da => da.Where(a => a.Task.Name == taskName &&                
+                                                                a.Resource.Name == localResourceName));   
+            context.Load(draft.ProjectResources, dp => dp.Where(r => r.Name == localResourceName));       
+            context.ExecuteQuery();
+
+            // Make sure the data on server is right
+            if (draft.Tasks.Count !=1 || draft.Assignments.Count != 1 || draft.ProjectResources.Count != 1)
+            {
+                Console.WriteLine("Failed to retrieve expected data, make sure you set up server data right. Press any key to continue....");
+                Console.ReadLine();
+                return;
+            }
+
+            // Since we already filetered and validated that the TaskCollection, ProjectResourceCollection and AssignmentCollection
+            // contains just one filtered item each, we just get the first one.
+            csom.DraftTask task = draft.Tasks.First();
+            csom.DraftProjectResource resource = draft.ProjectResources.First();
+            csom.DraftAssignment assignment = draft.Assignments.First();
+
+            // Update the project description
+            draft.Description += "(description updated)";
+
+            // Update task duration and start date
+            task.Duration = "10d";
+            task.Start = draft.StartDate.AddDays(3);
+            draft.Update();         // Save updates so far to ensure the task changes are applied before the assignment changes
+
+            // Update resource standard rate
+            resource.StandardRate = 100.0d;
+            draft.Update();
+
+            // Update assignment work percent complete
+            assignment.PercentWorkComplete = 50;
+
+            // Publish and check in the project
+            csom.JobState jobState = context.WaitForQueue(draft.Publish(true), DEFAULTTIMEOUTSECONDS);
+            JobStateLog(jobState, "Updating project");
+        }
+    }
+}
