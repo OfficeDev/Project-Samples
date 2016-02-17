8fd330798e328950e300efd826aea2de82a0a7f1
Yue Chi <yuechi@microsoft.com>
2016-02-17 15:00:36 -0800

    First Check In
    CSOM create project/update project/get, set custom field sample

---
+++ O365-Project-Online-CSharp-Samples/CreateProject.cs
@@ -0,0 +1,77 @@
+﻿using System;
+using System.IO;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+using csom = Microsoft.ProjectServer.Client;
+
+namespace CreateUpdateProjectSample
+{
+    partial class CreateUpdateProjectSample
+    {
+        
+
+        /// <summary>
+        /// Create a new project with one local resource, one enterprise resource, one task and one assignment
+        /// </summary>
+        public static void CreateProjectWithTaskAndAssignment()
+        {
+            //
+            // Load csom context
+            context = GetContext(pwaInstanceUrl);
+
+            //
+            // Create a project
+            csom.PublishedProject project = context.Projects.Add(new csom.ProjectCreationInformation()
+            {
+                Name = projectName,
+                Start = DateTime.Today,
+                Description = "Created project from C# library"
+            });
+            csom.JobState jobState = context.WaitForQueue(context.Projects.Update(), DEFAULTTIMEOUTSECONDS);
+            JobStateLog(jobState, "Creating project");
+
+            //
+            // Create a task in project
+            context.Load(project, p => p, 
+                                  p => p.StartDate);    //load startdate of project 
+            context.ExecuteQuery();
+
+            csom.DraftProject draft = project.CheckOut();
+            Guid taskId = Guid.NewGuid();
+            csom.Task task = draft.Tasks.Add(new csom.TaskCreationInformation()
+            {
+                Id = taskId,
+                Name = taskName,
+                IsManual = false,   
+                Start = project.StartDate.AddDays(1),
+                Duration = "5d"
+            });
+
+            draft.Update();
+
+            //
+            // Create a local resource and assign the task to him
+            Guid resourceId = Guid.NewGuid();
+            csom.ProjectResource resource = draft.ProjectResources.Add(new csom.ProjectResourceCreationInformation()
+            {
+                Id = resourceId,
+                Name = localResourceName
+            });
+
+            draft.Update();
+
+            csom.DraftAssignment assignment = draft.Assignments.Add(new csom.AssignmentCreationInformation()
+            {
+                ResourceId = resourceId,
+                TaskId = taskId
+            });
+
+            draft.Update();
+            jobState = context.WaitForQueue(draft.Publish(true), DEFAULTTIMEOUTSECONDS);    // draft.Publish(true) means publish and check in
+            JobStateLog(jobState, "Creating task and assgin to a local resource");
+        }
+    }
+}
--- README.md
+++ README.md
@@ -1,2 +1,30 @@
-# O365-Project-Online-CSOM-Dotnet-Project-Creation
-This Project Online add-in demonstrates how to create a project using DotNet with CSOM
+# Office 365 Project Online .NET Code Sample
+
+This Project Online .NET code sample demonstrates how to create and update a project using CSOM 
+
+###Prerequisites
+To use this Project Online ReST code sample, you need the following:
+* An Office 365 tenant with a Project license
+* The SharePoint client runtime and Project C# library (included in dll folder)
+* Visual Studio or other .Net IDE
+
+###Modules
+* [CreateProject](/CreateProject.cs)
+* [UpdateProject](/UpdateProject.cs)
+* [UpdateProjectCustomFields](/UpdateProjectCustomFieldValues.cs)
+
+##How the sample affects your tenant data
+This sample runs CSOM methods that create, read, update, or delete data. When running commands that delete or edit data, the sample creates sample data. The sample data is created or edited so that your actual tenant data is unaffected.
+
+## Questions and comments
+Please add the libraries in dll folder before running the sample. Or download the latest ones online(address in Addtional resources). How to add a reference please see
+(https://msdn.microsoft.com/en-us/library/7314433t(v=vs.90).aspx)
+
+## Additional resources
+* [PS namespace (ps.js)] (https://msdn.microsoft.com/en-us/library/office/jj669820.aspx)
+* [Client-side object model (CSOM) for Project 2013] (https://msdn.microsoft.com/en-us/library/office/jj163123.aspx)
+* [Project 2013 SDK] (https://www.microsoft.com/en-us/download/details.aspx?id=30435)
+* [SharePoint Online SDK] (https://www.microsoft.com/en-us/download/details.aspx?id=42038)
+
+## Copyright
+Copyright (c) 2016 Microsoft. All rights reserved.
